0e95ab131e12df0cdff59b321e8afcc27f1dc5a4
Ishrat Jahan Emu <35221540+ishrat98@users.noreply.github.com>
2018-03-06 23:55:58 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,58 +2,82 @@
 #include<sstream>
 #include<fstream>
 #include<vector>
+#include<bits/stdc++.h>
+
 using namespace std;
 
+void makeSmaller(string &word){
 
+	for(int i=0;i<word.length();i++)
+		{
+			if(word[i]>='A' && word[i]<='Z'){
 
-int main(){
+				word[i] = word[i]+32;
+		}
+
+	}
+}
+
+int main(void)
+{
 	vector<string> stopWordList;
 	ifstream iFile;
 	iFile.open("stopWord.txt");
 	string line,word;
 
 	while(getline(iFile,line)){
-
+		makeSmaller(line);
 		istringstream iLine(line);
 		while(iLine>>word){
+
 			stopWordList.push_back(word);
-			//cout<<word<<endl;
-
-		}
-	}
-	iFile.close();
-	vector <string> tokens;
-	//ifstream inputFile;
-	iFile.open("file.txt");
-	//string lines,words;
-
-	while(getline(iFile,line)){
-		istringstream inputLine(line);
-//		cleanUp(line);
-		while(inputLine>>word){
-		//while(getline(cin,words)){
-			tokens.push_back(word);
 
 
 		}
+	}
+
+
+	iFile.close();
+
+
+	vector <string> tokenization;
+	ifstream anotherFile;
+
+	anotherFile.open("smsDataset.txt");
+
+
+	while(getline(anotherFile,line)){
+
+		makeSmaller(line);
+
+		istringstream iLine(line);
+		while(iLine>>word)
+		{
+
+			tokenization.push_back(word);
+
+
+		}
 
 	}
-	iFile.close();
+	anotherFile.close();
 
 	vector<string> :: iterator it;
 	vector<string> :: iterator it2;
 
-
-	for(it = tokens.begin(); it!=tokens.end(); it++){
+	for(it = tokenization.begin(); it!=tokenization.end(); it++){
 		for(it2=stopWordList.begin(); it2!=stopWordList.end(); it2++){
 			if(*it == *it2){
-				tokens.erase(it);
+
+				tokenization.erase(it);
+				it--;
+
 			}
 		}
 	}
-	cout<<" after removing stopWord"<<endl;
-	for(it = tokens.begin(); it!=tokens.end(); it++){
-		cout<<*it<<endl;
+
+	for(it = tokenization.begin(); it!=tokenization.end(); it++){
+		cout << *it << endl;
 	}
 
 }
