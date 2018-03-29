f93e373404c392cde5f86ecdc2963744d2b750b6
Ishrat Jahan Emu <35221540+ishrat98@users.noreply.github.com>
2018-03-29 15:41:07 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 #include<vector>
 #include<bits/stdc++.h>
 
+#include "english_stem.h"  
+
+
 using namespace std;
+
+/*bool is_in_array(const wstring &value, const vector<wstring> &array)
+{
+    return find(array.begin(), array.end(), value) != array.end();
+};*/
 
 void makeSmaller(string &word){
 
@@ -17,6 +25,74 @@ void makeSmaller(string &word){
 
 	}
 }
+
+void printFrequency(string &str, char array[10000][10000], char array2[10000][10000])
+		{
+			c,coun ,j,k = 0;
+
+			int countSpace = 0;
+
+			for(int i=0;str[i]!='\0';i++){
+
+				if(str[i]==' '){
+
+				    countSpace++;
+			}
+		}
+		 //printf("Total spaces in st are %d\n", countSpace);
+
+
+		 for(int i =0; i <= countSpace; i++)
+		 {
+
+		 while(j<=countSpace)
+		 {
+			if(i==j)
+			{
+				strcpy(array2[k],array[i]);
+
+				k++;
+
+				coun++;
+
+				break;
+			}
+			else{
+				    if (strcmp(array2[j],array[i]) !=0)
+				    {
+				    j++;
+
+				    continue;
+
+				    }
+				else
+
+				    break;
+			}
+		}
+		 }
+		 for(int i = 0; i < coun-1 ; i++)
+		 {
+			 j =0;
+
+			 while(j <= countSpace)
+		 {
+			 if(strcmp(array2[i],array[j])==0)
+
+
+			 c++;
+
+			 j++;
+
+		 }
+
+		cout << array2[i] << " --> "<< c << "time(s)" << endl;
+		c=0;
+
+		 }
+		 cout<<endl;
+		}
+
 
 int main(void)
 {
@@ -76,8 +152,24 @@ int main(void)
 		}
 	}
 
-	for(it = tokenization.begin(); it!=tokenization.end(); it++){
+	/*for(it = tokenization.begin(); it!=tokenization.end(); it++){
 		cout << *it << endl;
-	}
+	}*/
+	
+	
+	stemming::english_stem<> stemObj; //creating an object of english_stem class
+    int i;
+    for(i=0; i<it.size(); i++){
+            theWord = it[i];
+            wcout<<"Original word was:\t"<<theWord.c_str()<<endl; //wcout is used to print wstring. c_str() is used to convert wstring to normal "c++" string
+            stemObj(theWord); //this method is stemming theWord variable and putting the stemmed word into it
+            wcout<<"Stemmed word is:\t"<<theWord.c_str()<<endl<<endl; //converting the stemmed word into normal string and printing
+        }
+        
+        
+    printFrequency(str,array,array2);
+         
+         
+    return 0;
 
 }
