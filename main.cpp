f737d6671a08ba0e350c9f1ac7c9f6b858a1ec4e
Ishrat Jahan Emu <35221540+ishrat98@users.noreply.github.com>
2018-04-04 16:05:12 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-#include<iostream>
-#include<sstream>
-#include<fstream>
-#include<vector>
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -22,15 +18,21 @@ void makeSmaller(string &s){
 
 int main(void)
 {
-	vector< map<string,int> > frequency;
+	vector< map<string,int> >frequency;
+	vector<int> hamSpam;
 	vector<string> stopWordList;
 	ifstream iFile;
 	iFile.open("stopWord.txt");
 	string line,word;
 
+	ofstream oFile;
+	oFile.open("out.txt");
+
+
 	while(getline(iFile,line)){
 		makeSmaller(line);
 		istringstream iLine(line);
+
 		while(iLine>>word){
 
 			stopWordList.push_back(word);
@@ -44,46 +46,52 @@ int main(void)
 	iFile.close();
 
 
-	vector< vector <string> > tokenization;
+	vector<vector <string> > tokenization;
 	ifstream anotherFile;
 
 	anotherFile.open("smsDataset.txt");
 
 
 	while(getline(anotherFile,line)){
-		vector<string> token;
+		vector <string> token;
+
 		makeSmaller(line);
 
 		istringstream iLine(line);
+		iLine>>word;
+		if(word=="ham") hamSpam.push_back(1);
+		else hamSpam.push_back(0);
 		while(iLine>>word)
 		{
 
 			token.push_back(word);
+
 		}
 		tokenization.push_back(token);
+
 	}
 	anotherFile.close();
 
 	vector<string> :: iterator it;
 	vector<string> :: iterator it2;
 
-
 	for(int i=0;i<tokenization.size();i++)
 	{
-		for(it = tokenization[i].begin(); it!=tokenization[i].end(); it++){
 
-			for(it2=stopWordList.begin(); it2!=stopWordList.end(); it2++){
-				if(*it == *it2){
 
-					tokenization[i].erase(it);
-					it--;
-				}
-			}
-		}
+        for(it2=stopWordList.begin(); it2!=stopWordList.end(); it2++){
+            for(it = tokenization[i].begin(); it!=tokenization[i].end(); it++)
+            {
+                if(*it == *it2){
+                    tokenization[i].erase(it);
+                    it--;
+
+                }
+            }
+        }
 	}
 
 	cout<<"All the words we have: "<<endl<<endl;
-
 	for(int i=0;i<tokenization.size();i++)
 	{
 		map<string,int> freq;
@@ -93,15 +101,14 @@ int main(void)
 		frequency.push_back(freq);
 	}
 
-	for(int i=0;i<tokenization.size();i++)
+	for(int i=0;i<frequency.size();i++)
 	{
-		cout<<"for message "<<i+1<<endl;
+	    //cout<<"for message no-"<<i+1<<endl;
+		oFile<<hamSpam[i];
 		for(map<string,int>::iterator iter=frequency[i].begin();iter!=frequency[i].end();iter++){
-			cout<<iter->first<<" --> "<<iter->second<<endl;
+			oFile<<" "<<iter->second;
 		}
-
+		oFile<<endl;
 	}
 
-
-	return 0;
 }
